42e7418332a52fcf68df6bb967a567cfa7f6d83f
aritradey-CS <aritra1999.dey@gmail.com>
2024-02-18 12:45:36 +0530

    Linkdin-clone using React.js+Vite

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBg-cZ7Re6G7HJieRrKFVi5eIrOCtcn2l0",
+  authDomain: "linkdin-clone-61b78.firebaseapp.com",
+  projectId: "linkdin-clone-61b78",
+  storageBucket: "linkdin-clone-61b78.appspot.com",
+  messagingSenderId: "442573158774",
+  appId: "1:442573158774:web:feff8af74f7f509a822257",
+  measurementId: "G-M3B63D4EQK"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
